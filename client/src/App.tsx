14ada89fb6d594311c376919cea25ad13ac905a0
Seth Anderson <sethdanderson17@gmail.com>
2021-06-12 18:38:43 -0700

    Start changing url based on colors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 //@ts-nocheck
 import chroma from "chroma-js";
 import React, { useState, useEffect } from "react";
-import { Switch, Route } from "react-router-dom";
+import {
+  Switch,
+  Route,
+  useHistory,
+  useParams,
+  useLocation,
+} from "react-router-dom";
 import "./App.css";
 import DemoPage from "./components/DemoPage";
 import LandingPage from "./components/LandingPage";
@@ -13,8 +19,8 @@ import colorService from "./services/colorService";
 
 interface IState {
   colors: {
-    color: string  
-  }[]
+    color: string;
+  }[];
 }
 
 const colorsFromBackend = [
@@ -25,26 +31,66 @@ const colorsFromBackend = [
   { color: "#6D5A72" },
 ];
 
-
 function App() {
-  const [colors, setColors] = useState<IState["colors"]>(colorsFromBackend);
-  const [colorMode, setColorMode] = useState<string>('monochrome')
+  let history = useHistory();
+  const location = useLocation();
+  console.log("location", location);
+
+  const [colors, setColors] = useState<IState["colors"]>([]);
+  const [colorMode, setColorMode] = useState<string>(null);
+
+  const hasColorSlug = true;
+
+  const getColorSlug = (colors) => {
+    const colorSlug = location.pathname.split("/")[2];
+    if (colorSlug.length) {
+      if (colors.length) {
+        return colors
+          .map((el) => el.color)
+          .join("")
+          .replaceAll("#", "-")
+          .slice(1);
+      }
+      return colorSlug;
+    } else {
+      return "5EFC8D-7F270E-AE3512-DD4216-ED6139";
+    }
+  };
+
+  const colorObjFromSlug = (colorSlug) => {
+    const colors = [];
+    colorSlug.split("-").forEach((el) => colors.push({ color: "#" + el }));
+    return colors;
+  };
+
+  const currentColors = (colors) => {
+    if (hasColorSlug) {
+      const slug = getColorSlug(colors);
+      colors = colorObjFromSlug(slug);
+    } else {
+      colors = colorsFromBackend;
+    }
+    return colors;
+  };
 
   useEffect(() => {
-    generatePalette()
-  },[colorMode])
+    setColors(currentColors(colors));
+  }, []);
+
+  useEffect(() => {
+    generatePalette();
+  }, [colorMode]);
 
   const generatePalette = async () => {
-    let first = chroma.random().hex().substring(1)
-    let response = colorService.getPalette(first, colorMode, colors.length)
-    let returnedArray = (await response).data.colors
-    let newArray = []
-    for(let i = 0; i<returnedArray.length; i++){
-      newArray.push({color: returnedArray[i].hex.value})
+    let first = chroma.random().hex().substring(1);
+    let response = colorService.getPalette(first, colorMode, colors.length);
+    let returnedArray = (await response).data.colors;
+    let newArray = [];
+    for (let i = 0; i < returnedArray.length; i++) {
+      newArray.push({ color: returnedArray[i].hex.value });
     }
-    setColors(newArray)
-    console.log(newArray)
-
+    setColors(newArray);
+    console.log("newArray", newArray);
   };
 
   const addColor = () => {
@@ -55,6 +101,15 @@ function App() {
     setColors((colors) => [...colors, newColor]);
   };
 
+  useEffect(() => {
+    const colorSlug = getColorSlug(colors);
+    history.push(`/palette/${colorSlug}`);
+  }, [colors]);
+
+  if (colors.length === 0) {
+    return null;
+  }
+
   return (
     <div className="App">
       <Navbar />
@@ -62,7 +117,7 @@ function App() {
         <Route exact path="/">
           <LandingPage />
         </Route>
-        <Route exact path="/palette">
+        <Route exact path={`/palette/:${getColorSlug(colors)}`}>
           <PalettePage
             colors={colors}
             setColors={setColors}
@@ -71,8 +126,12 @@ function App() {
             generatePalette={generatePalette}
           />
         </Route>
-        <Route exact path="/demo">
-          <DemoPage colors={colors} setColorMode={setColorMode} generatePalette={generatePalette}/>
+        <Route exact path={`/demo/:${getColorSlug(colors)}`}>
+          <DemoPage
+            colors={colors}
+            setColorMode={setColorMode}
+            generatePalette={generatePalette}
+          />
         </Route>
         <Route exact path="/templates">
           <TemplatesPage />
