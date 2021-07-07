cd576c862d9a0d4eea0017050cd757bdeb2e6081
Nelson Su <nelson.su@fdmgroup.com>
2021-07-07 17:49:01 -0400

    Added github and local authentication

--- server/controllers/colorPaletteRouter.js
+++ server/controllers/colorPaletteRouter.js
@@ -1,59 +1,96 @@
 const colorPaletteRouter = require("express").Router()
 const ColorPalette = require('../models/ColorPalette.model')
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt');
+
+//env
+const SECRET = 'chingubears3';
 
 colorPaletteRouter.get("/", async(request, response) => {
-    const colorPalettes = await ColorPalette.find({});
-    response.json(colorPalettes.map((colorPalette) => colorPalette.toJSON()));
+    var token = request.headers['access_token'];
+    if (!token) 
+        return response.status(401).send("No token found");
+    jwt.verify(token, SECRET, async function(err, decoded) {
+        if (err) 
+            return res.status(500).send('Failed to authenticate token');
+
+        const colorPalettes = await ColorPalette.find({});
+        response.json(colorPalettes.map((colorPalette) => colorPalette.toJSON()));
+    });
 })
 
 colorPaletteRouter.get("/:id", async(request, response) => {
-    console.log('getting by id')
-    const colorPalette = await ColorPalette.findOne({colorPaletteID: request.params.id});
+    var token = request.headers['access_token'];
+    if (!token) 
+        return response.status(401).send("No token found");
+    jwt.verify(token, SECRET, async function(err, decoded) {
+        if (err) 
+            return response.status(500).send('Failed to authenticate token');
 
-    if(colorPalette) {
-        response.json(colorPalette.toJSON());
-    } else {
-        response.status(404).end();
-    }
+        console.log('getting by id')
+        const colorPalette = await ColorPalette.findOne({colorPaletteID: request.params.id});
+
+        if(colorPalette) {
+            response.json(colorPalette.toJSON());
+        } else {
+            response.status(404).end();
+        }
+    });
 })
 
 colorPaletteRouter.post("/generate", async(request, response) => {
-    const body = request.body;
+    var token = request.headers['access_token'];
+    if (!token) 
+        return response.status(401).send("No token found");
+    jwt.verify(token, SECRET, async function(err, decoded) {
+        if (err) 
+            return response.status(500).send('Failed to authenticate token');
 
-    const existingPalette = await ColorPalette.findOne({colorPaletteID: request.body.colorPaletteID});
+        const body = request.body;
 
-    if (existingPalette) {
-        response.json({error: "color palette already generated"})
-    } else {
+        const existingPalette = await ColorPalette.findOne({colorPaletteID: request.body.colorPaletteID});
 
-        const colorPalette = new ColorPalette({
-            colors: body.colors,
-            likes: body.likes || 0,
-            colorPaletteID: body.colorPaletteID,
-            tags: [] || body.tags
-        });
-    
-        await colorPalette.save();
-    
-        console.log(colorPalette);
-        response.json(colorPalette);
-    }
+        if (existingPalette) {
+            response.json({error: "color palette already generated"})
+        } else {
+
+            const colorPalette = new ColorPalette({
+                colors: body.colors,
+                likes: body.likes || 0,
+                colorPaletteID: body.colorPaletteID,
+                tags: [] || body.tags
+            });
+        
+            await colorPalette.save();
+        
+            console.log(colorPalette);
+            response.json(colorPalette);
+        }
+    });
 });
 
 colorPaletteRouter.put("/:id/update", async(request, response) => {
-    const body = request.body;
-    const colorPalette = await ColorPalette.findOne({colorPaletteID: request.body.colorPaletteID});
-    console.log(colorPalette);
-    
-    colorPalette.colors = body.colors;
-    colorPalette.likes = body.likes;
-    colorPalette.colorPaletteID = body.colorPaletteID;
-    colorPalette.tags = body.tags;
+    var token = request.headers['access_token'];
+    if (!token) 
+        return response.status(401).send("No token found");
+    jwt.verify(token, SECRET, async function(err, decoded) {
+        if (err) 
+            return response.status(500).send('Failed to authenticate token');
 
-    console.log(colorPalette);
+        const body = request.body;
+        const colorPalette = await ColorPalette.findOne({colorPaletteID: request.body.colorPaletteID});
+        console.log(colorPalette);
+        
+        colorPalette.colors = body.colors;
+        colorPalette.likes = body.likes;
+        colorPalette.colorPaletteID = body.colorPaletteID;
+        colorPalette.tags = body.tags;
 
-    await colorPalette.save();
-    response.json(colorPalette);
+        console.log(colorPalette);
+
+        await colorPalette.save();
+        response.json(colorPalette);
+    });
 })
 
 module.exports = colorPaletteRouter;
